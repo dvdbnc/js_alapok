9fed0d471c7bbc37dbd85627bb8855e64524db7d
Unknown <bence.david13@gmail.com>
2018-02-16 09:30:21 +0100

    folytatas
    gallery,diplay none

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
 /*jslint devel: true */
+/*global $, jQuery, alert*/
+/*jslint indent: 2 */
 
-console.log("hello word");
 
-$('.image-popup').magnificPopup({
-    type: 'image'
-});
+(function () {
+  'use strict';
+  console.log("hello world");
+  $('.image-popup').magnificPopup({
+    type: 'image',
+    gallery: {
+    enabled: true
+    }
+  });
+}());
+
+//[].forEach.call(document.querySelectorAll('.image-popup'),
+//               function (el) {
+//    el.style.display = 'none';
+//});
+
+$('.image-popup').css('display','none');
+
+
+
